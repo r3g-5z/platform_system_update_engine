TeamWin/android_system_update_engine

Add SafetyNet logging for payload timestamp error.
Bug: 36232423
Test: mma

Change-Id: Ica752e24e78e84350e13c66a2a0a56092f232d0b

update_attempter_android.cc
@@ -29,6 +29,7 @@
 #include <brillo/data_encoding.h>
 #include <brillo/message_loops/message_loop.h>
 #include <brillo/strings/string_utils.h>
+#include <log/log_safetynet.h>
 
 #include "update_engine/common/constants.h"
 #include "update_engine/common/file_fetcher.h"
@@ -327,6 +328,11 @@ void UpdateAttempterAndroid::ProcessingDone(const ActionProcessor* processor,
       // beginning next time.
       DeltaPerformer::ResetUpdateProgress(prefs_, false);
       LOG(INFO) << "Resetting update progress.";
+      break;
+
+    case ErrorCode::kPayloadTimestampError:
+      // SafetyNet logging, b/36232423
+      android_errorWriteLog(0x534e4554, "36232423");
       break;
 
     default:

r3g-5z/platform_system_update_engine

Merged-In: Ica752e24e78e84350e13c66a2a0a56092f232d0b
(cherry picked from commit b75539b3ee05aa07b107fd711f010af54a80b0f2)
(cherry picked from commit c2edd69550306c42b8140ffe3035111fcbe52181)
Exempt-From-Owner-Approval: OWNERS file outdated in nyc-*
(cherry picked from commit 570a360a0b267f953d5274ca3952ffa23a4f64ba)

update_attempter_android.cc
@@ -26,6 +26,7 @@
 #include <brillo/bind_lambda.h>
 #include <brillo/message_loops/message_loop.h>
 #include <brillo/strings/string_utils.h>
+#include <log/log.h>
 
 #include "update_engine/common/constants.h"
 #include "update_engine/common/file_fetcher.h"
@@ -300,6 +301,11 @@ void UpdateAttempterAndroid::ProcessingDone(const ActionProcessor* processor,
       // beginning next time.
       DeltaPerformer::ResetUpdateProgress(prefs_, false);
       LOG(INFO) << "Resetting update progress.";
+      break;
+
+    case ErrorCode::kPayloadTimestampError:
+      // SafetyNet logging, b/36232423
+      android_errorWriteLog(0x534e4554, "36232423");
       break;
 
     default: